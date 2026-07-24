9d51a1a4e8af529ea08f5a756d67a34ff8ab175f
yazoink <yazoink@firemail.cc>
2026-07-24 22:06:36 +1000

    something

--- static/.config/eww/control_center/scripts/player.sh
+++ static/.config/eww/control_center/scripts/player.sh
@@ -7,8 +7,8 @@ case "$1" in
         eww update song_status="$(control_center/scripts/get_player_status.sh)"
         ;;
     "next" | "prev") 
-        eww update song="$(playerctl metadata title | echo "n/a")"
-        eww update song_artist="$(playerctl metadata artist | echo "n/a")"
+        eww update song="$(playerctl metadata title)"
+        eww update song_artist="$(playerctl metadata artist)"
         eww update cover_art="$(control_center/scripts/get_album_cover.sh)"
         ;;
 esac
