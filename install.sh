686718137f30b0fc798b57f1587bf86234f7fd45
yazoink <yazoink@firemail.cc>
2026-08-08 01:54:26 +1000

    improve installer

--- install.sh
+++ install.sh
@@ -55,8 +55,8 @@ function enable_chaotic_aur() {
             sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
             sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
             echo "
-    [chaotic-aur]
-    Include = /etc/pacman.d/chaotic-mirrorlist
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
             " | sudo tee -a /etc/pacman.conf
             sudo pacman -Syu
         fi
