9fccbe81f8626e69c412d0595802888b24eada60
yazoink <yazoink@firemail.cc>
2026-08-07 22:48:18 +1000

    something

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO: optionally install nvim and deps, graphics drivers?
+# TODO: optionally install nvim and deps, fix waybar workspaces menu, graphics 
+# drivers?
 
 TARGET_DIR=~/.local/share/yzshell
 SRC_DIR=/tmp/yzshell
