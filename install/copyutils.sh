7d164ae607344e0a7f53ca5c5fb0f3f92b2a67a0
yazoink <yazoink@firemail.cc>
2026-08-07 17:00:52 +1000

    something

--- install/copyutils.sh
+++ install/copyutils.sh
@@ -1,4 +1,5 @@
 function copy_data_dir() {
+    local -; set -e
     src="${SRC_DIR}/${1}"
     target="${TARGET_DIR}/${1}"
     cp -rf "$src" "$target"
@@ -6,6 +7,7 @@ function copy_data_dir() {
 }
 
 function copy_executable() {
+    local -; set -e
     src="${SRC_DIR}/bin/${1}"
     target="/usr/bin/${1}"
     sudo install -Dm755 "$src" "$target"
