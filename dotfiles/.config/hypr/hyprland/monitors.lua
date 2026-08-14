1a3ac0ac3c7465dc097e37586df78821b368f6c5
yazoink <yazoink@firemail.cc>
2026-08-14 14:42:11 +1000

    add my monitor to hyprland conf

--- dotfiles/.config/hypr/hyprland/monitors.lua
+++ dotfiles/.config/hypr/hyprland/monitors.lua
@@ -4,3 +4,10 @@ hl.monitor({
     position = "auto",
     scale    = "auto",
 })
+
+hl.monitor({
+  output = "desc:Dell Inc. DELL U2715H 6VY7R72K01YS",
+  mode = "preferred",
+  position = "auto",
+  scale = 1.25,
+})
