9b419e6da883168871c94ebc72677e048d2f167a
yazoink <yazoink@firemail.cc>
2026-08-15 21:37:35 +1000

    something

--- install.sh
+++ install.sh
@@ -185,6 +185,7 @@ function main() {
         deps_import_gpg_keys
         install_pkgs "${DEPS[@]}"
         install_aur_pkgs "${AUR_DEPS[@]}"
+        sudo systemctl enable --now swayosd-libinput-backend.service
     fi
     # INSTALL OPTIONAL DEPS
     if [ $install_opt_deps -eq 0 ]; then
