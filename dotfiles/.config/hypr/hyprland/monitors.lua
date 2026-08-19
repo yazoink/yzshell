f393c724773c971d3c635fae5b50dec083f9b2b1
yazoink <yazoink@firemail.cc>
2026-08-19 11:59:16 +1000

    something

--- dotfiles/.config/hypr/hyprland/monitors.lua
+++ dotfiles/.config/hypr/hyprland/monitors.lua
@@ -9,6 +9,6 @@ hl.monitor({
   output = "desc:Dell Inc. DELL U2715H 6VY7R72K01YS",
   mode = "preferred",
   position = "auto",
-  --scale = 1.33,
-  scale = 1.6,
+  scale = 1.33,
+  --scale = 1.6,
 })
