0c9d4c8f41e0703ee347292d2db4d0a96af5c84d
yazoink <yazoink@firemail.cc>
2026-09-03 23:20:03 +1000

    something

--- dotfiles/.config/hypr/hyprland/scripts/idle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-swayidle -w \
-    timeout 300 'hyprlock --grace 30' \
-    timeout 600 "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'" \
-    resume "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'" \
-    before-sleep 'hyprlock --grace 30'
