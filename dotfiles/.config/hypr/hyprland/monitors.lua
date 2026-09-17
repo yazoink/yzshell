9847c436aa03a376a8ee657cf9b1d8ac05a0cfe0
yazoink <yazoink@firemail.cc>
2026-09-17 18:48:02 +1000

    something

--- dotfiles/.config/hypr/hyprland/monitors.lua
+++ dotfiles/.config/hypr/hyprland/monitors.lua
@@ -1,14 +1,21 @@
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "auto",
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
 })
 
 hl.monitor({
-  output = "desc:Dell Inc. DELL U2715H 6VY7R72K01YS",
-  mode = "preferred",
-  position = "auto",
-  scale = 1.33,
-  --scale = 1.6,
+	output = "desc:Dell Inc. DELL U2715H 6VY7R72K01YS",
+	mode = "preferred",
+	position = "auto",
+	scale = 1.33,
+	--scale = 1.6,
+})
+
+hl.monitor({
+	output = "desc:BOE 0x0868",
+	mode = "preferred",
+	position = "auto",
+	scale = 1,
 })
