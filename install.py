e2e6175077ab05ddd8d8b4fb8560f89fa2f0df6f
yazoink <yazoink@firemail.cc>
2026-09-19 21:43:30 +1000

    something

--- install.py
+++ install.py
@@ -180,15 +180,17 @@ def enable_chaotic_aur():
             sudo pacman-key --init
             sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
             sudo pacman-key --lsign-key 3056513887B78AEB
-            sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-            sudo pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+            sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+            sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
             echo "
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
             " | sudo tee -a /etc/pacman.conf
             """,
             shell=True,
-            stdout=subprocess.PIPE
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
         )
         update_pacman()
         announce("chaotic-aur repo enabled!")
