9a5fe1931d1bd7a2cf65d13c14b2a34434447374
yazoink <yazoink@firemail.cc>
2026-09-19 21:35:29 +1000

    something

--- install.py
+++ install.py
@@ -175,13 +175,22 @@ def delete_if_exists(p):
 
 def enable_chaotic_aur():
     if confirm("enable chaotic-aur repo? (recommended)") == True:
+        chaotic_pkgs = [
+            "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst",
+            "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+        ]
         subprocess.run(
             """
             sudo pacman-key --init
             sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
             sudo pacman-key --lsign-key 3056513887B78AEB
-            sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-            sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+            """,
+            shell=True
+        )
+        for p in chaotic_pkgs:
+            subprocess.run(["sudo", "pacman", "-U", p])
+        subprocess.run(
+            """
             echo "
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
